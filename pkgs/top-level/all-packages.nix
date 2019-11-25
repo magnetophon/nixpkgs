10509d291b1d494bba527b78325fd0718d67546d
Bart Brouns <bart@magnetophon.nl>
2019-11-25 22:55:29 +0100

    noise-repellent: init at unstable-29-12-2018

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5263,6 +5263,9 @@ in
 
   nnn = callPackage ../applications/misc/nnn { };
 
+
+  noise-repellent = callPackage ../applications/audio/noise-repellent { };
+
   notary = callPackage ../tools/security/notary { };
 
   notify-osd = callPackage ../applications/misc/notify-osd { };
