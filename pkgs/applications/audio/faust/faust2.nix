85318d0b9d561c5b6bb0bcc408aa4565783534bd
Bart Brouns <bart@magnetophon.nl>
2020-08-06 21:42:17 +0200

    WIP faust

--- pkgs/applications/audio/faust/faust2.nix
+++ pkgs/applications/audio/faust/faust2.nix
@@ -5,7 +5,8 @@
 , pkgconfig
 , cmake
 , llvm
-, emscripten
+# TODO: put back when it builds again
+# , emscripten
 , openssl
 , libsndfile
 , libmicrohttpd
@@ -14,19 +15,22 @@
 , p11-kit
 , vim
 , which
+, libpthreadstubs
+, libjack2
+, gtk2
 }:
 
 with stdenv.lib.strings;
 
 let
 
-  version = "unstable-2020-06-08";
+  version = "unstable-2020-08-03";
 
   src = fetchFromGitHub {
     owner = "grame-cncm";
     repo = "faust";
-    rev = "f0037e289987818b65d3f6fb1ad943aaad2a2b28";
-    sha256 = "0h08902rgx7rhzpng4h1qw8i2nzv50f79vrlbzdk5d35wa4zibh4";
+    rev = "b6045f4592384076d3b383d116e602a95a000eb3";
+    sha256 = "1wcpilwnkc7rrbv9gbkj5hb7kamkh8nrc3r4hbcvbz5ar2pfc6d5";
     fetchSubmodules = true;
   };
 
@@ -46,7 +50,7 @@ let
     inherit src;
 
     nativeBuildInputs = [ makeWrapper pkgconfig cmake vim which ];
-    buildInputs = [ llvm emscripten openssl libsndfile libmicrohttpd gnutls libtasn1 p11-kit ];
+    buildInputs = [ llvm /*emscripten*/ openssl libsndfile libmicrohttpd gnutls libtasn1 p11-kit libpthreadstubs libjack2 gtk2];
 
 
     passthru = {
@@ -62,10 +66,14 @@ let
       -C ../backends/all.cmake -C  ../targets/all.cmake ..
     '';
 
+
     postInstall = ''
       # syntax error when eval'd directly
       pattern="faust2!(*@(atomsnippets|graph|graphviewer|md|plot|sig|sigviewer|svg))"
       (shopt -s extglob; rm "$out"/bin/$pattern)
+      cd ../..
+      make benchmark -j
+      make devinstall
     '';
 
     postFixup = ''
