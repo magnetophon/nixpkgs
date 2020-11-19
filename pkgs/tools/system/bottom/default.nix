d93681fa37ca1e5f4e6feaec9b69006459b41298
Bart Brouns <bart@magnetophon.nl>
2020-11-19 22:33:37 +0100

    bottom: 0.4.7 -> 0.5.0

--- pkgs/tools/system/bottom/default.nix
+++ pkgs/tools/system/bottom/default.nix
@@ -2,18 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "bottom";
-  version = "0.4.7";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "ClementTsang";
     repo = pname;
     rev = version;
-    sha256 = "rDcJ5XF7L13MKZ8/J4sYD+UqC+HkZvxRtDkY9IVLH50=";
+    sha256 = "0swcd7j4bq2v1817qp9x9ybz16c39rvrlc81p8lb47qk0kxnxc67";
   };
 
   buildInputs = stdenv.lib.optional stdenv.hostPlatform.isDarwin darwin.apple_sdk.frameworks.IOKit;
 
-  cargoSha256 = "XeX6QM0a628mcaptNZkKAvDnGfW5tx+aWNBpMyjz44M=";
+  cargoSha256 = "1p9c23z2nzhi1pklnn7zs3px7p076w0285l77mih1i4zayzwxa4q";
 
   doCheck = false;
 
