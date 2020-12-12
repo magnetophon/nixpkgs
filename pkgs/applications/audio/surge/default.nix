3268461220a2b84e55bb25d50aadf204837bcd0e
Bart Brouns <bart@magnetophon.nl>
2020-12-12 15:04:56 +0100

    surge: WIP

--- pkgs/applications/audio/surge/default.nix
+++ pkgs/applications/audio/surge/default.nix
@@ -4,13 +4,16 @@
 
 stdenv.mkDerivation rec {
   pname = "surge";
-  version = "1.7.1";
+  version = "unstable-2020-12-11";
+  # version = "1.7.1";
 
   src = fetchFromGitHub {
     owner = "surge-synthesizer";
     repo = pname;
-    rev = "release_${version}";
-    sha256 = "1b3ccc78vrpzy18w7070zfa250dnd1bww147xxcnj457vd6n065s";
+    # rev = "release_${version}";
+    # sha256 = "1b3ccc78vrpzy18w7070zfa250dnd1bww147xxcnj457vd6n065s";
+    rev = "dafcd600ce044c8d860660bee449b85fa047a98e";
+    sha256 = "1agp1r19hgh96r31rqsis1ja0a64g5nic723grz382gnk4izwiw2";
     leaveDotGit = true; # for SURGE_VERSION
     fetchSubmodules = true;
   };
@@ -20,7 +23,7 @@ stdenv.mkDerivation rec {
 
   postPatch = ''
     substituteInPlace src/common/SurgeStorage.cpp --replace "/usr/share/Surge" "$out/share/surge"
-    substituteInPlace src/common/gui/PopupEditorDialog.cpp --replace '"zenity' '"${zenity}/bin/zenity'
+    # substituteInPlace src/common/gui/PopupEditorDialog.cpp --replace '"zenity' '"${zenity}/bin/zenity'
     substituteInPlace src/linux/UserInteractionsLinux.cpp --replace '"zenity' '"${zenity}/bin/zenity'
     substituteInPlace vstgui.surge/vstgui/lib/platform/linux/x11fileselector.cpp --replace /usr/bin/zenity ${zenity}/bin/zenity
   '';
