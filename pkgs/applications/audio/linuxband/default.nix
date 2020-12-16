a461c9b143ed9dff0370785e4358829583c1400b
Bart Brouns <bart@magnetophon.nl>
2020-12-16 22:28:45 +0100

    WIP linuxband

--- pkgs/applications/audio/linuxband/default.nix
+++ pkgs/applications/audio/linuxband/default.nix
@@ -1,23 +1,35 @@
-{ stdenv, fetchurl, makeWrapper, pkgconfig, MMA, libjack2, libsmf, python2Packages }:
+{ stdenv, fetchFromGitHub, makeWrapper, autoreconfHook, pkgconfig, MMA, libjack2, libsmf, python2Packages }:
 
 let
   inherit (python2Packages) pyGtkGlade pygtksourceview python;
 in stdenv.mkDerivation rec {
-  version = "12.02.1";
   pname = "linuxband";
+  version = "unstable-2020-02-23";
+  # version = "12.02.1";
 
-  src = fetchurl {
-    url = "http://linuxband.org/assets/sources/${pname}-${version}.tar.gz";
-    sha256 = "1r71h4yg775m4gax4irrvygmrsclgn503ykmc2qwjsxa42ri4n2n";
+  # src = fetchurl {
+  # url = "http://linuxband.org/assets/sources/${pname}-${version}.tar.gz";
+  # sha256 = "1r71h4yg775m4gax4irrvygmrsclgn503ykmc2qwjsxa42ri4n2n";
+  # };
+
+  src = fetchFromGitHub {
+    owner = "noseka1";
+    repo = pname;
+    rev = "e5459ccc75feaa9bd03d3a0d74af1b81f62825ce";
+    sha256 = "0mlxrrz1c0qbpbx1y5ilkl33mdv6smwxj7l7n89j6bh6462lzpmf";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig ];
   buildInputs = [ makeWrapper MMA libjack2 libsmf python pyGtkGlade pygtksourceview ];
 
   patchPhase = ''
     sed -i 's@/usr/@${MMA}/@g' src/main/config/linuxband.rc.in
-    cat src/main/config/linuxband.rc.in
+    # cat src/main/config/linuxband.rc.in
+ls
+cat autogen.sh
   '';
+
+  # preConfigure = "autoreconf --install";
 
   postFixup = ''
     PYTHONPATH=$pyGtkGlade/share/:pygtksourceview/share/:$PYTHONPATH
