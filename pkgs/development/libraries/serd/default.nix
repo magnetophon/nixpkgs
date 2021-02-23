308a29717215be944fee76bbbf013950fe17a320
Bart Brouns <bart@magnetophon.nl>
2021-02-23 18:13:18 +0100

    serd: 0.30.4 -> 0.30.8

--- pkgs/development/libraries/serd/default.nix
+++ pkgs/development/libraries/serd/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "serd";
-  version = "0.30.4";
+  version = "0.30.8";
 
   src = fetchurl {
     url = "https://download.drobilla.net/${pname}-${version}.tar.bz2";
-    sha256 = "168rn3m32c59qbar120f83ibcnnd987ij9p053kybgl7cmm6358c";
+    sha256 = "11zs53yx40mv62vxsl15mvdh7s17y5v6lgcgahdvzxgnan7w8bk7";
   };
 
   nativeBuildInputs = [ pkgconfig python3 wafHook ];
