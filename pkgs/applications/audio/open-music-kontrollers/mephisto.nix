0ac0e5569385309be15239e7f534555dcecc0514
Bart Brouns <bart@magnetophon.nl>
2021-02-23 17:58:20 +0100

    mephisto: 0.12.0 -> 0.14.0

--- pkgs/applications/audio/open-music-kontrollers/mephisto.nix
+++ pkgs/applications/audio/open-music-kontrollers/mephisto.nix
@@ -1,12 +1,12 @@
-{ callPackage, faust, fontconfig, cmake, libvterm-neovim, libevdev, libglvnd, fira-code, ... } @ args:
+{ callPackage, faust, fontconfig, cmake, libvterm-neovim, libevdev, libglvnd, fira, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
   pname = "mephisto";
-  version = "0.12.0";
+  version = "0.14.0";
+  sha256 = "1wcn31016dj6q90zk3wjv1x4v3gbrj7ywz14yw4f194505y9hkp1";
 
-  sha256 = "1k9l6n4r45qrigkwqr06jzq42w3dz7j20vhs5pbl6v1nahc8jayz";
 
-  additionalBuildInputs = [ faust fontconfig cmake libvterm-neovim libevdev libglvnd fira-code ];
+  additionalBuildInputs = [ faust fontconfig cmake libvterm-neovim libevdev libglvnd fira ];
 
   description = "A Just-in-time FAUST embedded in an LV2 plugin";
   broken = true;
