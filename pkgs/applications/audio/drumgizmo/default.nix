ae68c0c11edec1d6a54bb6a02fd44f123dae7876
Bart Brouns <bart@magnetophon.nl>
2021-04-09 23:20:35 +0200

    drumgizmo: 0.9.18.1 -> 0.9.19

--- pkgs/applications/audio/drumgizmo/default.nix
+++ pkgs/applications/audio/drumgizmo/default.nix
@@ -3,23 +3,13 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "0.9.18.1";
+  version = "0.9.19";
   pname = "drumgizmo";
 
   src = fetchurl {
     url = "https://www.drumgizmo.org/releases/${pname}-${version}/${pname}-${version}.tar.gz";
-    sha256 = "0bpbkzcr3znbwfdk79c14n5k5hh80iqlk2nc03q95vhimbadk8k7";
+    sha256 = "18x28vhif0c97xz02k22xwqxxig6fi6j0356mlz2vf7vb25z69kl";
   };
-
-  patches = [
-    # Fix build for lv2 1.18.0
-    (fetchpatch {
-      url = "http://cgit.drumgizmo.org/plugingizmo.git/patch/?id=be64ddf9da525cd5c6757464efc966052731ba71";
-      sha256 = "17w8g78i5avssc7m8rpw64ka3rai8dff81wfzir9cpxp8s2h44qf";
-      extraPrefix = "plugin/plugingizmo/";
-      stripLen = 1;
-    })
-  ];
 
   configureFlags = [ "--enable-lv2" ];
 
