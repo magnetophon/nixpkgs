6b1e04db14c61d903f0e42a35d7761e48a4ffe2e
Bart Brouns <bart@magnetophon.nl>
2021-04-09 23:31:46 +0200

    surge: unstable-2021-04-08

--- pkgs/applications/audio/surge/default.nix
+++ pkgs/applications/audio/surge/default.nix
@@ -1,44 +1,59 @@
-{ lib, stdenv, fetchFromGitHub, cmake, git, pkg-config, python3
+{ stdenv, fetchurl, fetchFromGitHub, cmake, git, pkg-config, python3
 , cairo, libsndfile, libxcb, libxkbcommon, xcbutil, xcbutilcursor, xcbutilkeysyms, zenity
+, curl, rsync
 }:
 
 stdenv.mkDerivation rec {
   pname = "surge";
-  version = "1.7.1";
+  # version = "1.8.1";
+  version = "unstable-2021-04-08";
+
+  # src = fetchurl {
+  # url = "https://github.com/surge-synthesizer/releases/releases/download/${version}/SurgeSrc_${version}.tgz";
+  # sha256 = "1nakblkrig10816ixmhlb6wcfj1ki1wx7k68qnanbpdw0wmwy7y2";
+  # };
 
   src = fetchFromGitHub {
     owner = "surge-synthesizer";
-    repo = pname;
-    rev = "release_${version}";
-    sha256 = "1b3ccc78vrpzy18w7070zfa250dnd1bww147xxcnj457vd6n065s";
-    leaveDotGit = true; # for SURGE_VERSION
+    repo = "surge";
+    rev = "d20cbe49e25906dd9de33da1c1e0ec8aa6635623";
+    sha256 = "1wgymjfvj3shhvhx139hlrihmmnnd0cn6im6arz18d5jsy539dlz";
+    leaveDotGit = true;
     fetchSubmodules = true;
   };
 
+  extraContent = fetchFromGitHub {
+    owner = "surge-synthesizer";
+    repo = "surge-extra-content";
+    # rev from: https://github.com/surge-synthesizer/surge/blob/release_1.8.1/cmake/stage-extra-content.cmake#L6
+    # SURGE_EXTRA_CONTENT_HASH
+    rev = "a4265856ae98f34a5b4e38ce8f36d29b646f56ec";
+    sha256 = "119b55h7prw3id5fnpgc3zpildddnr7r48glzczcp65v7qawxi4y";
+  };
   nativeBuildInputs = [ cmake git pkg-config python3 ];
-  buildInputs = [ cairo libsndfile libxcb libxkbcommon xcbutil xcbutilcursor xcbutilkeysyms zenity ];
+  buildInputs = [ cairo libsndfile libxcb libxkbcommon xcbutil xcbutilcursor xcbutilkeysyms zenity curl rsync ];
 
   postPatch = ''
     substituteInPlace src/common/SurgeStorage.cpp --replace "/usr/share/Surge" "$out/share/surge"
-    substituteInPlace src/common/gui/PopupEditorDialog.cpp --replace '"zenity' '"${zenity}/bin/zenity'
     substituteInPlace src/linux/UserInteractionsLinux.cpp --replace '"zenity' '"${zenity}/bin/zenity'
-    substituteInPlace vstgui.surge/vstgui/lib/platform/linux/x11fileselector.cpp --replace /usr/bin/zenity ${zenity}/bin/zenity
+    patchShebangs scripts/linux/
+    # cp -r $extraContent/Skins/ resources/data/skins
   '';
 
+
   installPhase = ''
-    mkdir -p $out/lib/lv2 $out/lib/vst3 $out/share/surge
-    cp -r surge_products/Surge.lv2 $out/lib/lv2/
-    cp -r surge_products/Surge.vst3 $out/lib/vst3/
-    cp -r ../resources/data/* $out/share/surge/
+    cd ..
+    cmake --build build --config Release --target install-everything-global
   '';
 
   doInstallCheck = true;
   installCheckPhase = ''
-    cd ..
+    export HOME=$(mktemp -d)
+    export SURGE_DISABLE_NETWORK_TESTS=TRUE
     build/surge-headless
   '';
 
-  meta = with lib; {
+  meta = with stdenv.lib; {
     description = "LV2 & VST3 synthesizer plug-in (previously released as Vember Audio Surge)";
     homepage = "https://surge-synthesizer.github.io";
     license = licenses.gpl3;
