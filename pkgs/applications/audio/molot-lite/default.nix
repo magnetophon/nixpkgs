a5de35fcd091d00afd8f38f0be6e8da209ca339a
Bart Brouns <bart@magnetophon.nl>
2021-05-22 03:25:12 +0200

    molot-lite: use fork with bugfix
    The original only had the mone version working; the stereo one just
    passed the sound.
    
    https://github.com/magnetophon/molot-lite/commit/06c055a94709f06ebc2537eb45bdfd45cd9a5127

--- pkgs/applications/audio/molot-lite/default.nix
+++ pkgs/applications/audio/molot-lite/default.nix
@@ -1,35 +1,29 @@
-{ lib, stdenv, fetchurl, unzip, lv2 }:
+{ lib, stdenv, fetchFromGitHub, unzip, lv2 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
+
   pname = "molot-lite";
-  version = "unstable-2014-04-23";
+  version = "unstable-2021-05-22";
 
-  src = fetchurl {
-    # fetchzip does not accept urls that do not end with .zip.
-    url = "https://sourceforge.net/p/molot/code/ci/c4eddc426f8d5821e8ebcf1d67265365e4c8c52a/tree/molot_src.zip?format=raw";
-    sha256 = "1c47dwfgrmn9459px8s5zikcqyr0777v226qzcxlr6azlcjwr51b";
+  src = fetchFromGitHub {
+    owner = "magnetophon";
+    repo = pname;
+    rev = "938e7b293d5eb930d3cadf31a92cb5fadf4168f2";
+    sha256 = "1dbq21g732qr80cyn6xqd0ki92d034ywmsb76dv68yxx5ifh97km";
   };
 
-  nativeBuildInputs = [ unzip ];
   buildInputs = [ lv2 ];
-
-  unpackPhase = ''
-    unzip $src
-  '';
-
-  buildPhase = ''
-    make -C Molot_Mono_Lite
-    make -C Molot_Stereo_Lite
-  '';
 
   installPhase = ''
     make install INSTALL_DIR=$out/lib/lv2 -C Molot_Mono_Lite
     make install INSTALL_DIR=$out/lib/lv2 -C Molot_Stereo_Lite
   '';
 
+  makeFlags = [ "INSTALL_DIR=$out/lib/lv2" ];
+
   meta = with lib; {
     description = "Stereo and mono audio signal dynamic range compressor in LV2 format";
-    homepage = "https://sourceforge.net/projects/molot/";
+    homepage = "https://github.com/magnetophon/molot-lite";
     license = licenses.gpl3Plus;
     maintainers = [ maintainers.magnetophon ];
     platforms = platforms.linux;
