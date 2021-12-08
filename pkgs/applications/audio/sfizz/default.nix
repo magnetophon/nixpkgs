72bc2d01959a0b3d53d636e9d1eb1fa641957820
Bart Brouns <bart@magnetophon.nl>
2021-12-08 21:45:01 +0100

    WIP sfizz

--- pkgs/applications/audio/sfizz/default.nix
+++ pkgs/applications/audio/sfizz/default.nix
@@ -1,18 +1,25 @@
-{ lib, stdenv, fetchFromGitHub
-, libjack2, libsndfile, xorg, freetype, libxkbcommon
-, cairo, glib, gnome, flac, libogg, libvorbis, libopus
-, cmake, pkg-config
+{ lib, stdenv, fetchFromGitHub, libjack2, libsndfile, libsamplerate, xorg
+, freetype, libxkbcommon, cairo, pango, glib, gnome, flac, libogg, libvorbis
+, libopus, cmake, pkg-config
+# , makeWrapper
+, pcre             # libpcre required by glib-2.0
+, utillinux        # mount required by gio-2.0
+, libselinux       # libselinux required by gio-2.0
+, libsepol         # libsepol required by libselinx
+, fribidi
+, libthai          # libthai required by pango
+, libdatrie        # libdatrie  required by libthai
 }:
 
 stdenv.mkDerivation rec {
   pname = "sfizz";
-  version = "0.5.1";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "sfztools";
     repo = pname;
     rev = version;
-    sha256 = "sha256-3RdY5+BPsdk6vctDy24w5aJsVOV9qzSgXs62Pm5UEKs=";
+    sha256 = "sha256-9khAx3nkcigGq/TobL0GYmvrX3zQbyndf8/IhLRd978=";
     fetchSubmodules = true;
   };
 
@@ -33,22 +40,36 @@ stdenv.mkDerivation rec {
     xorg.xcbutilkeysyms
     xorg.xcbutilimage
     libxkbcommon
+    libsamplerate
     cairo
+    pango
     glib
     gnome.zenity
     freetype
+    pcre
+    utillinux
+    libselinux
+    libsepol
+    fribidi
+    libthai
+    libdatrie
   ];
-  nativeBuildInputs = [ cmake pkg-config ];
+
+  nativeBuildInputs = [ cmake pkg-config
+                        # makeWrapper
+                      ];
 
   postPatch = ''
-  substituteInPlace editor/external/vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp \
-    --replace '"/usr/bin/zenity' '"${gnome.zenity}/bin/zenity'
+    substituteInPlace plugins/editor/src/editor/NativeHelpers.cpp \
+    --replace '/usr/bin/zenity' '${gnome.zenity}/bin/zenity'
   '';
 
-  cmakeFlags = [
-    "-DCMAKE_BUILD_TYPE=Release"
-    "-DSFIZZ_TESTS=ON"
-  ];
+  # postInstall = ''
+  # wrapProgram $out/bin/sfizz_jack --prefix PATH ":" ${gnome.zenity}/bin
+  # wrapProgram $out/bin/sfizz_render --prefix PATH ":" ${gnome.zenity}/bin
+  # '';
+
+  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DSFIZZ_TESTS=ON" ];
 
   meta = with lib; {
     homepage = "https://github.com/sfztools/sfizz";
