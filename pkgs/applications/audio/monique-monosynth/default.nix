75fa5782c99f904f38ce88eb284c785a7e2e0430
Bart Brouns <bart@magnetophon.nl>
2021-12-25 12:22:53 +0100

    monique-monosynth: standalone working

--- pkgs/applications/audio/monique-monosynth/default.nix
+++ pkgs/applications/audio/monique-monosynth/default.nix
@@ -16,12 +16,12 @@
 
 stdenv.mkDerivation rec {
   pname = "monique-monosynth";
-  version = "unstable-2021-12-18";
+  version = "unstable-2021-12-24";
   src = fetchFromGitHub {
     owner = "surge-synthesizer";
     repo = "monique-monosynth";
-    rev = "d175dbb037f3c477e59db0cc8d4a50e349e5e10f";
-    sha256 = "sha256-GswWnp7btJkj2kbAnvzqmwHJS1Cuvpz2cgfYcL8Jd20=";
+    rev = "8b38ba2159ef6cc789f1e4225395a8d4bb2d2e9f";
+    sha256 = "sha256-ba3CqRdn2qxAJBODj5mkF5iP3/jRv7EMVxw/cVWcSq4=";
     fetchSubmodules = true;
   };
 
@@ -42,12 +42,34 @@ stdenv.mkDerivation rec {
     libXrandr
   ];
 
-  # Ignore undefined references to a bunch of libsoup symbols
-  NIX_LDFLAGS = "--unresolved-symbol=ignore-all";
+  # JUCE dlopen's these at runtime, crashes without them
+  NIX_LDFLAGS = (toString [
+    "-lX11"
+    "-lXext"
+    "-lXcursor"
+    "-lXinerama"
+    "-lXrandr"
+    "-ljack -L${libjack2}"
+  ]);
+
+  patchPhase = ''
+    substituteInPlace CMakeLists.txt \
+      --replace "juce::juce_recommended_config_flags" "" \
+      --replace "juce::juce_recommended_lto_flags" "" \
+      --replace "# cmake options" "add_compile_options(
+        -fvisibility=hidden
+        -fvisibility-inlines-hidden
+       )"
+  '';
+
+  preConfigure = ''
+    cmakeFlagsArray+=(-DCMAKE_EXE_LINKER_FLAGS="-no-pie")
+  '' ;
 
   installPhase = ''
-    mkdir -p $out/lib/vst3
+    mkdir -p $out/lib/vst3 $out/bin
     cp -r MoniqueMonosynth_artefacts/Release/VST3/MoniqueMonosynth.vst3 $out/lib/vst3
+    cp -r MoniqueMonosynth_artefacts/Release/Standalone/MoniqueMonosynth $out/bin/
   '';
 
   meta = {
