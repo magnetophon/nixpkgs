a453611d2c9bfa0a6cfe73453bc29be3634321cc
Bart Brouns <bart@magnetophon.nl>
2022-01-08 20:48:24 +0100

    Cardinal: init at unstable-2022-01-14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24551,6 +24551,8 @@ with pkgs;
 
   carddav-util = callPackage ../tools/networking/carddav-util { };
 
+  cardinal = callPackage ../applications/audio/cardinal { };
+
   carla = libsForQt5.callPackage ../applications/audio/carla { };
 
   castor = callPackage ../applications/networking/browsers/castor { };
