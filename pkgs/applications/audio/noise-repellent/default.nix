6a0d888e9863a57458fcd6789161d888ab3dbf5c
Bart Brouns <bart@magnetophon.nl>
2022-04-25 22:12:11 +0200

    noise-repellent: 0.1.5 -> 0.2.1

--- pkgs/applications/audio/noise-repellent/default.nix
+++ pkgs/applications/audio/noise-repellent/default.nix
@@ -1,23 +1,29 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, fftwFloat, lv2 }:
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, cmake, libspecbleach, lv2 }:
 
 stdenv.mkDerivation rec {
   pname = "noise-repellent";
-  version = "0.1.5";
+  version = "0.2.1";
 
   src = fetchFromGitHub {
     owner = "lucianodato";
     repo = pname;
-    rev = version;
-    sha256 = "0hb89x9i2knzan46q4nwscf5zmnb2nwf4w13xl2c0y1mx1ls1mwl";
+    rev = "v${version}";
+    sha256 = "sha256-hMNVzhJZFGFeu5aygLkfq495O0zpaIk41ddzejvDITE=";
     fetchSubmodules = true;
   };
 
   mesonFlags = ("--prefix=${placeholder "out"}/lib/lv2");
 
-  nativeBuildInputs = [ meson ninja pkg-config ];
+  nativeBuildInputs = [ meson ninja pkg-config cmake ];
   buildInputs = [
-    fftwFloat lv2
+    libspecbleach lv2
   ];
+
+  # See: https://github.com/lucianodato/noise-repellent/issues/99
+  postInstall = ''
+    mv $out/lib/lv2/.lv2/nrepellent.lv2 $out/lib/lv2/nrepellent.lv2
+    rm -rf $out/lib/lv2/.lv2/
+  '';
 
   meta = with lib; {
     description = "An lv2 plugin for broadband noise reduction";
