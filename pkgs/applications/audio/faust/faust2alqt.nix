13c5701359b564e6a9ce696943774cced35a080a
Bart Brouns <bart@magnetophon.nl>
2023-04-03 16:04:07 +0200

    faust: replace qt4 with qt5

--- pkgs/applications/audio/faust/faust2alqt.nix
+++ pkgs/applications/audio/faust/faust2alqt.nix
@@ -1,15 +1,43 @@
 { faust
 , alsa-lib
-, qt4
+, qtbase
+, writeText
+, makeWrapper
 }:
 
-faust.wrapWithBuildEnv {
+faust.wrapWithBuildEnv rec {
 
   baseName = "faust2alqt";
 
   propagatedBuildInputs = [
     alsa-lib
-    qt4
+    qtbase
   ];
 
+  dontWrapQtApps = true;
+
+  # Wrap the binary coming out of the the compilation script, so it knows QT_PLUGIN_PATH
+  wrapBinary = writeText "wrapBinary" ''
+    source ${makeWrapper}/nix-support/setup-hook
+    for p in $FILES; do
+      workpath=$(pwd)
+      cd -- "$(dirname "$p")"
+      binary=$(basename --suffix=.dsp "$p")
+      mv $binary ."$binary"-wrapped
+      makeWrapper ./."$binary"-wrapped ."$binary"-temp  --set QT_PLUGIN_PATH "${qtbase}/${qtbase.qtPluginPrefix}"
+      mv ."$binary"-temp $binary
+      sed -i $binary -e 's@exec@cd "$(dirname "$(readlink -f "''${BASH_SOURCE[0]}")")" \&\& exec@g'
+      cd $workpath
+    done
+  '';
+
+  # append the wrapping code to the compilation script (cat line)
+  # prevent the qmake error when running the script (sed line)
+  preFixup = ''
+    for script in "$out"/bin/*; do
+      echo $script
+      cat ${wrapBinary} >> $script
+      sed -i "/QMAKE=/c\ QMAKE="${qtbase.dev}/bin/qmake"" $script
+    done
+  '';
 }
--- pkgs/applications/audio/faust/faust2jaqt.nix
+++ pkgs/applications/audio/faust/faust2jaqt.nix
@@ -1,12 +1,14 @@
 { faust
 , jack2
-, qt4
+, qtbase
 , libsndfile
 , alsa-lib
+, writeText
+, makeWrapper
 , which
 }:
 
-faust.wrapWithBuildEnv {
+faust.wrapWithBuildEnv rec {
 
   baseName = "faust2jaqt";
 
@@ -17,10 +19,35 @@ faust.wrapWithBuildEnv {
 
   propagatedBuildInputs = [
     jack2
-    qt4
+    qtbase
     libsndfile
     alsa-lib
     which
   ];
 
+  dontWrapQtApps = true;
+
+  # Wrap the binary coming out of the the compilation script, so it knows QT_PLUGIN_PATH
+  wrapBinary = writeText "wrapBinary" ''
+    source ${makeWrapper}/nix-support/setup-hook
+    for p in $FILES; do
+      workpath=$(pwd)
+      cd -- "$(dirname "$p")"
+      binary=$(basename --suffix=.dsp "$p")
+      mv $binary ."$binary"-wrapped
+      makeWrapper ./."$binary"-wrapped ."$binary"-temp  --set QT_PLUGIN_PATH "${qtbase}/${qtbase.qtPluginPrefix}"
+      mv ."$binary"-temp $binary
+      sed -i $binary -e 's@exec@cd "$(dirname "$(readlink -f "''${BASH_SOURCE[0]}")")" \&\& exec@g'
+      cd $workpath
+    done
+  '';
+  # append the wrapping code to the compilation script (cat line)
+  # prevent the qmake error when running the script (sed line)
+  preFixup = ''
+    for script in "$out"/bin/*; do
+      echo $script
+      cat ${wrapBinary} >> $script
+      sed -i "/QMAKE=/c\ QMAKE="${qtbase.dev}/bin/qmake"" $script
+    done
+  '';
 }
