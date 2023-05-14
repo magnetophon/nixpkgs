c58fc0b3e10ffbe09920fe24575d6d3b9e942450
Bart Brouns <bart@magnetophon.nl>
2023-05-14 11:39:03 +0200

    ConstantDetuneChorus: 0.1.3 -> 0.1.5

--- pkgs/applications/audio/magnetophonDSP/ConstantDetuneChorus/default.nix
+++ pkgs/applications/audio/magnetophonDSP/ConstantDetuneChorus/default.nix
@@ -1,27 +1,24 @@
 { lib, stdenv, fetchFromGitHub, faust2jaqt, faust2lv2 }:
 stdenv.mkDerivation rec {
   pname = "constant-detune-chorus";
-  version = "0.1.3";
+  version = "0.1.5";
 
   src = fetchFromGitHub {
     owner = "magnetophon";
     repo = "constant-detune-chorus";
-    rev = "V${version}";
-    sha256 = "1sipmc25fr7w7xqx1r0y6i2zwfkgszzwvhk1v15mnsb3cqvk8ybn";
+    rev = version;
+    sha256 = "sha256-wLAUy21OxGBCwMrp8upjlV6JVsE0YGva6ZR84FrDPV4=";
   };
 
   buildInputs = [ faust2jaqt faust2lv2 ];
 
-  buildPhase = ''
-    faust2jaqt -time -vec -t 99999 ConstantDetuneChorus.dsp
-    faust2lv2  -time -vec -t 99999 -gui ConstantDetuneChorus.dsp
-  '';
+  makeFlags = [ "PREFIX=$(out)" ];
+  enableParallelBuilding = true;
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ConstantDetuneChorus $out/bin/
-    mkdir -p $out/lib/lv2
-    cp -r ConstantDetuneChorus.lv2/ $out/lib/lv2
+  postInstall = ''
+    for f in $(find . -executable -type f -name '*-wrapped'); do
+      cp $f $out/bin/
+    done
   '';
 
   meta = {
