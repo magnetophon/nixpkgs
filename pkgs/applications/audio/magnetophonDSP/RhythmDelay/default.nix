ac03ef2226c0d35f66f73da3c2eb35a57219b7ef
Bart Brouns <bart@magnetophon.nl>
2023-05-14 12:24:35 +0200

    RhythmDelay: 2.1 -> 2.2.0

--- pkgs/applications/audio/magnetophonDSP/RhythmDelay/default.nix
+++ pkgs/applications/audio/magnetophonDSP/RhythmDelay/default.nix
@@ -1,27 +1,23 @@
 { lib, stdenv, fetchFromGitHub, faust2jaqt, faust2lv2 }:
 stdenv.mkDerivation rec {
   pname = "RhythmDelay";
-  version = "2.1";
+  version = "2.2.0";
 
   src = fetchFromGitHub {
     owner = "magnetophon";
     repo = "RhythmDelay";
-    rev = "V${version}";
-    sha256 = "1j0bjl9agz43dcrcrbiqd7fv7xsxgd65s4ahhv5pvcr729y0fxg4";
+    rev = version;
+    sha256 = "sha256-NjuowEBfAshhSCz7thGWz5u2DNhbvrMcx1iRjePdK9A=";
   };
 
   buildInputs = [ faust2jaqt faust2lv2 ];
 
-  buildPhase = ''
-    faust2jaqt -time -vec -t 99999 RhythmDelay.dsp
-    faust2lv2  -time -vec -t 99999 -gui RhythmDelay.dsp
-  '';
+  makeFlags = [ "PREFIX=$(out)" ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp RhythmDelay $out/bin/
-    mkdir -p $out/lib/lv2
-    cp -r RhythmDelay.lv2/ $out/lib/lv2
+  postInstall = ''
+    for f in $(find . -executable -type f -name '*-wrapped'); do
+      cp $f $out/bin/
+    done
   '';
 
   meta = {
