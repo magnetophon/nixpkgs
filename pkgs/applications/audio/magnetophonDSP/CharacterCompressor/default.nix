2f97adf48a825c0b56cba71e7f255bc520de670b
Bart Brouns <bart@magnetophon.nl>
2023-05-15 17:28:21 +0200

    CharacterCompressor: 0.3.3 -> 0.3.4

--- pkgs/applications/audio/magnetophonDSP/CharacterCompressor/default.nix
+++ pkgs/applications/audio/magnetophonDSP/CharacterCompressor/default.nix
@@ -1,37 +1,31 @@
 { lib, stdenv, fetchFromGitHub, faust2jaqt, faust2lv2 }:
 stdenv.mkDerivation rec {
   pname = "CharacterCompressor";
-  version = "0.3.3";
+  version = "0.3.4";
 
   src = fetchFromGitHub {
     owner = "magnetophon";
     repo = "CharacterCompressor";
-    rev = "V${version}";
-    sha256 = "1h0bhjhx023476gbijq842b6f8z71zcyn4c9mddwyb18w9cdamp5";
+    rev = version;
+    sha256 = "sha256-PwiNHeqcpg8K4K2ohUdw0sfibtEYESg3RllibVOb+Cw=";
   };
 
   buildInputs = [ faust2jaqt faust2lv2 ];
 
-  buildPhase = ''
-    faust2jaqt -vec -time -t 99999 CharacterCompressor.dsp
-    faust2jaqt -vec -time -t 99999 CharacterCompressorMono.dsp
-    faust2lv2 -vec -time -gui -t 99999 CharacterCompressor.dsp
-    faust2lv2 -vec -time -gui -t 99999 CharacterCompressorMono.dsp
-  '';
+  makeFlags = [ "PREFIX=$(out)" ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp CharacterCompressor $out/bin/
-    cp CharacterCompressorMono $out/bin/
-    mkdir -p $out/lib/lv2
-    cp -r CharacterCompressor.lv2/ $out/lib/lv2
-    cp -r CharacterCompressorMono.lv2/ $out/lib/lv2
+  enableParallelBuilding = true;
+
+  postInstall = ''
+    for f in $(find . -executable -type f -name '*-wrapped'); do
+      cp $f $out/bin/
+    done
   '';
 
   meta = {
     description = "A compressor with character. For jack and lv2";
     homepage = "https://github.com/magnetophon/CharacterCompressor";
-    license = lib.licenses.gpl3;
+    license = lib.licenses.gpl2;
     maintainers = [ lib.maintainers.magnetophon ];
   };
 }
