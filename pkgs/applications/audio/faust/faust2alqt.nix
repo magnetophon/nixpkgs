2e5298423a04c7d401b72a9b67973f70f5844a59
Bart Brouns <bart@magnetophon.nl>
2023-05-17 12:22:43 +0200

    wip faust: sandros suggestions

--- pkgs/applications/audio/faust/faust2alqt.nix
+++ pkgs/applications/audio/faust/faust2alqt.nix
@@ -4,8 +4,23 @@
 , writeText
 , makeWrapper
 }:
-
-faust.wrapWithBuildEnv rec {
+let
+  # Wrap the binary coming out of the the compilation script, so it knows QT_PLUGIN_PATH
+  wrapBinary = writeText "wrapBinary" ''
+    source ${makeWrapper}/nix-support/setup-hook
+    for p in $FILES; do
+      workpath=$PWD
+      cd -- "$(dirname "$p")"
+      binary=$(basename --suffix=.dsp "$p")
+      mv $binary ."$binary"-wrapped
+      makeWrapper "./.$binary-wrapped" ".$binary-temp" --set QT_PLUGIN_PATH "${qtbase}/${qtbase.qtPluginPrefix}"
+      mv ".$binary-temp" "$binary"
+      sed -i $binary -e 's@exec@cd "$(dirname "$(readlink -f "''${BASH_SOURCE[0]}")")" \&\& exec@g'
+      cd $workpath
+    done
+  '';
+in
+faust.wrapWithBuildEnv {
 
   baseName = "faust2alqt";
 
@@ -16,27 +31,11 @@ faust.wrapWithBuildEnv rec {
 
   dontWrapQtApps = true;
 
-  # Wrap the binary coming out of the the compilation script, so it knows QT_PLUGIN_PATH
-  wrapBinary = writeText "wrapBinary" ''
-    source ${makeWrapper}/nix-support/setup-hook
-    for p in $FILES; do
-      workpath=$(pwd)
-      cd -- "$(dirname "$p")"
-      binary=$(basename --suffix=.dsp "$p")
-      mv $binary ."$binary"-wrapped
-      makeWrapper ./."$binary"-wrapped ."$binary"-temp  --set QT_PLUGIN_PATH "${qtbase}/${qtbase.qtPluginPrefix}"
-      mv ."$binary"-temp $binary
-      sed -i $binary -e 's@exec@cd "$(dirname "$(readlink -f "''${BASH_SOURCE[0]}")")" \&\& exec@g'
-      cd $workpath
-    done
-  '';
-
-  # append the wrapping code to the compilation script (cat line)
-  # prevent the qmake error when running the script (sed line)
   preFixup = ''
     for script in "$out"/bin/*; do
-      echo $script
+      # append the wrapping code to the compilation script
       cat ${wrapBinary} >> $script
+      # prevent the qmake error when running the script
       sed -i "/QMAKE=/c\ QMAKE="${qtbase.dev}/bin/qmake"" $script
     done
   '';
--- pkgs/applications/audio/faust/faust2jaqt.nix
+++ pkgs/applications/audio/faust/faust2jaqt.nix
@@ -7,8 +7,23 @@
 , makeWrapper
 , which
 }:
-
-faust.wrapWithBuildEnv rec {
+let
+  # Wrap the binary coming out of the the compilation script, so it knows QT_PLUGIN_PATH
+  wrapBinary = writeText "wrapBinary" ''
+    source ${makeWrapper}/nix-support/setup-hook
+    for p in $FILES; do
+      workpath=$PWD
+      cd -- "$(dirname "$p")"
+      binary=$(basename --suffix=.dsp "$p")
+      mv $binary ."$binary"-wrapped
+      makeWrapper "./.$binary-wrapped" ".$binary-temp" --set QT_PLUGIN_PATH "${qtbase}/${qtbase.qtPluginPrefix}"
+      mv ".$binary-temp" "$binary"
+      sed -i $binary -e 's@exec@cd "$(dirname "$(readlink -f "''${BASH_SOURCE[0]}")")" \&\& exec@g'
+      cd $workpath
+    done
+  '';
+in
+faust.wrapWithBuildEnv {
 
   baseName = "faust2jaqt";
 
@@ -27,26 +42,11 @@ faust.wrapWithBuildEnv rec {
 
   dontWrapQtApps = true;
 
-  # Wrap the binary coming out of the the compilation script, so it knows QT_PLUGIN_PATH
-  wrapBinary = writeText "wrapBinary" ''
-    source ${makeWrapper}/nix-support/setup-hook
-    for p in $FILES; do
-      workpath=$(pwd)
-      cd -- "$(dirname "$p")"
-      binary=$(basename --suffix=.dsp "$p")
-      mv $binary ."$binary"-wrapped
-      makeWrapper ./."$binary"-wrapped ."$binary"-temp  --set QT_PLUGIN_PATH "${qtbase}/${qtbase.qtPluginPrefix}"
-      mv ."$binary"-temp $binary
-      sed -i $binary -e 's@exec@cd "$(dirname "$(readlink -f "''${BASH_SOURCE[0]}")")" \&\& exec@g'
-      cd $workpath
-    done
-  '';
-  # append the wrapping code to the compilation script (cat line)
-  # prevent the qmake error when running the script (sed line)
   preFixup = ''
     for script in "$out"/bin/*; do
-      echo $script
+      # append the wrapping code to the compilation script
       cat ${wrapBinary} >> $script
+      # prevent the qmake error when running the script
       sed -i "/QMAKE=/c\ QMAKE="${qtbase.dev}/bin/qmake"" $script
     done
   '';
