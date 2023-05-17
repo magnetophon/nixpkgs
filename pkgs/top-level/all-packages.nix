08d7534054b19a7fea6c2ccd1e96d677e891efc7
Bart Brouns <bart@magnetophon.nl>
2023-05-17 20:28:40 +0200

    remove faustStk

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38644,8 +38644,6 @@ with pkgs;
 
   faustPhysicalModeling = callPackage ../applications/audio/faustPhysicalModeling  { };
 
-  faustStk = callPackage ../applications/audio/faustStk  { };
-
   flockit = callPackage ../tools/backup/flockit { };
 
   fahclient = callPackage ../applications/science/misc/foldingathome/client.nix { };
