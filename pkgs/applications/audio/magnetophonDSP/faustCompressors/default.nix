439feb81390be81baf837f6ec6af5ed1ac6276be
Bart Brouns <bart@magnetophon.nl>
2023-05-17 18:30:07 +0200

    faustCompressors: 1.2 -> 1.2.1

--- pkgs/applications/audio/magnetophonDSP/faustCompressors/default.nix
+++ pkgs/applications/audio/magnetophonDSP/faustCompressors/default.nix
@@ -1,44 +1,27 @@
 { lib, stdenv, fetchFromGitHub, faust2jaqt, faust2lv2 }:
 stdenv.mkDerivation rec {
   pname = "faustCompressors";
-  version = "1.2";
+  version = "1.2.1";
 
   src = fetchFromGitHub {
     owner = "magnetophon";
     repo = "faustCompressors";
-    rev = "v${version}";
-    sha256 = "144f6g17q4m50kxzdncsfzdyycdfprnpwdaxcwgxj4jky1xsha1d";
+    rev = version;
+    sha256 = "sha256-nUKDHaXBZ6pu+H/xKaku/CaB99/k89lXLaHLgB6oHvU=";
   };
 
   buildInputs = [ faust2jaqt faust2lv2 ];
 
+  makeFlags = [ "PREFIX=$(out)" ];
   dontWrapQtApps = true;
+  enableParallelBuilding = true;
 
-  buildPhase = ''
-    echo "hack out autoComp.dsp due to https://github.com/grame-cncm/faust/407/issues "
-    rm autoComp.dsp
-    for f in *.dsp;
-    do
-      echo "compiling standalone from" $f
-      faust2jaqt -time -double -t 99999 $f
-    done
-
-    for f in *.dsp;
-    do
-      echo "Compiling plugin from" $f
-      faust2lv2  -time -double -gui -t 99999 $f
-    done
-  '';
-
-  installPhase = ''
-    mkdir -p $out/lib/lv2
-    mv *.lv2/ $out/lib/lv2
-    mkdir -p $out/bin
-    rm newlib.sh
-    for f in $(find . -executable -type f); do
+  postInstall = ''
+    for f in $(find . -executable -type f -name '*-wrapped'); do
       cp $f $out/bin/
     done
   '';
+
 
   meta = {
     description = "A collection of bread and butter compressors";
