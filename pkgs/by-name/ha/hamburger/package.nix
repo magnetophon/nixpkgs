3b2cba79dc25a88dc764a0b27be4187c6b2ba9ee
Bart Brouns <bart@magnetophon.nl>
2025-10-30 16:59:00 +0100

    wip 3

--- pkgs/by-name/ha/hamburger/package.nix
+++ pkgs/by-name/ha/hamburger/package.nix
@@ -2,10 +2,11 @@
   alsa-lib,
   cmake,
   curl,
+  expat,
+  fontconfig,
   libepoxy,
   fetchFromGitHub,
   freetype,
-  juce,
   lib,
   libGL,
   libXcursor,
@@ -21,26 +22,41 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "hamburger";
   version = "0.5";
-  
+
   src = fetchFromGitHub {
     owner = "Davit-G";
     repo = "Hamburger";
     tag = "v${finalAttrs.version}";
     hash = "sha256-U/xy+uYTtDnBEHuZmVQkjTxy191oyLRs+ofnE/sHMPA=";
-    fetchSubmodules = true;
+  };
+
+  juce-src = fetchFromGitHub {
+    owner = "juce-framework";
+    repo = "JUCE";
+    rev = "8.0.4";
+    hash = "sha256-iAueT+yHwUUHOzqfK5zXEZQ0GgOKJ9q9TyRrVfWdewc="; # Run to get hash
+  };
+
+  clap-juce-extensions-src = fetchFromGitHub {
+    owner = "free-audio";
+    repo = "clap-juce-extensions";
+    rev = "4d454e5125da75a0e75d95615cbec26d2a09e2bf";
+    hash = "sha256-+i4w6NOImWkxNIOEKFN5houclbOmtgH6FNvGz+LHw9M="; # Run to get hash
+    fetchSubmodules = true; # This one likely has submodules
   };
 
   nativeBuildInputs = [
     pkg-config
     cmake
   ];
-  
+
   buildInputs = [
     alsa-lib
     curl
+    expat
+    fontconfig
     libepoxy
     freetype
-    juce
     libGL
     libXcursor
     libXext
@@ -51,13 +67,32 @@ stdenv.mkDerivation (finalAttrs: {
     lv2
   ];
 
+
+  postUnpack = ''
+    mkdir -p source/modules
+    cp -r ${finalAttrs.juce-src} source/modules/JUCE
+    cp -r ${finalAttrs.clap-juce-extensions-src} source/modules/clap-juce-extensions
+    chmod -R u+w source/modules
+  '';
+
   postPatch = ''
-    # Remove all FetchContent declarations and calls
+    # Remove all FetchContent lines
     sed -i '/FetchContent_Declare/,/^)/d' CMakeLists.txt
     sed -i '/FetchContent_MakeAvailable/d' CMakeLists.txt
     
-    # Add JUCE as a subdirectory (assuming juce package provides source)
-    sed -i '/^project(/a add_subdirectory(${juce.src} juce-build EXCLUDE_FROM_ALL)' CMakeLists.txt
+    # Add subdirectories after the project() declaration
+    sed -i '/^project(/a \
+    add_subdirectory(modules/JUCE)\
+    add_subdirectory(modules/clap-juce-extensions)' CMakeLists.txt
+    
+    # Add proper include path configuration
+    cat <<EOF >> CMakeLists.txt
+    target_include_directories(Hamburger PUBLIC 
+      \$\{CMAKE_CURRENT_SOURCE_DIR}/modules/JUCE
+      \$\{CMAKE_CURRENT_SOURCE_DIR}/modules/clap-juce-extensions
+      \$\{CMAKE_CURRENT_SOURCE_DIR}/dsp
+    )
+    EOF
   '';
 
   meta = {
